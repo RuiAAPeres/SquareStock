b158ebd1b7ceab7992ac936e97cb7b4a396d338c
Rui Peres <rui.peres@just-eat.com>
2015-05-05 02:18:22 +0100

    Removed cocoapods, started using Carthage

--- Pods/Target Support Files/Pods-SquareStockTests-LlamaKit/Pods-SquareStockTests-LlamaKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SquareStockTests_LlamaKit : NSObject
-@end
-@implementation PodsDummy_Pods_SquareStockTests_LlamaKit
-@end
--- Pods/Target Support Files/Pods-SquareStockTests-Nimble/Pods-SquareStockTests-Nimble-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SquareStockTests_Nimble : NSObject
-@end
-@implementation PodsDummy_Pods_SquareStockTests_Nimble
-@end
