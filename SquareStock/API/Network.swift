33d3fa806a1d5e6a447e9613f06d72dc54e60ef4
Rui Peres <rui.peres@just-eat.com>
2015-05-05 20:55:37 +0100

    Added the URLRequest creation method

--- SquareStock/API/Network.swift
+++ SquareStock/API/Network.swift
@@ -9,9 +9,25 @@
 import Foundation
 import ReactiveCocoa
 
+let baseURL  = "https://squarestock.herokuapp.com/api/v2/"
+
 func fetchStockFromNetwork (market : Market) -> SignalProducer<[Market], NSError>
 {
     return SignalProducer { sink, disposable in
-        
+        NSURLConnection.sendAsynchronousRequest(urlRequest(market), queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            
+        }
     }
+}
+
+private func urlRequest (market : Market ) -> NSURLRequest
+{
+    let path : String
+    
+    switch market
+    {
+        case .NASDAQ100 : path = "nasdaq100"
+    }
+    
+    return NSURLRequest(URL: NSURL(string: baseURL + path)!)
 }
